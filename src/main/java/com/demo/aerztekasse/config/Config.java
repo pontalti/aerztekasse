3573a07fdf2e17dc48cc39ed79d8b05bacb49566
Gustavo Pontalti <gustavopontalti@msn.com>
2025-05-21 11:30:59 +0200

    add config and bin/ to .gitignore

---
+++ src/main/java/com/demo/aerztekasse/config/Config.java
@@ -0,0 +1,24 @@
+package com.demo.aerztekasse.config;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public List<DayOfWeek> dayOrder(){
+        return List.of(DayOfWeek.MONDAY,
+                        DayOfWeek.TUESDAY,
+                        DayOfWeek.WEDNESDAY,
+                        DayOfWeek.THURSDAY,
+                        DayOfWeek.FRIDAY,
+                        DayOfWeek.SATURDAY,
+                        DayOfWeek.SUNDAY);
+    }
+
+
+}
